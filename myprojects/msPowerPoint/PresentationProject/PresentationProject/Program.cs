204743e4777715f723703d9964f1d9371688e317
olivier hess <olivier@blackreflect.com>
2015-06-21 15:13:28 +0100

    Initial commit

---
+++ myprojects/msPowerPoint/PresentationProject/PresentationProject/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Drawing; 
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.IO.Packaging;
+using System.IO;
+using DocumentFormat.OpenXml.Presentation;
+//using Microsoft.Office.Interop.PowerPoint;
+using A = DocumentFormat.OpenXml.Drawing;
+using P = DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Office2010.Drawing; 
+
+namespace PresentationProject
+{
+    class Program
+    {
+        protected static System.Int64 cY;
+        protected static int Count;
+
+        protected static string headerText;
+        protected static string contentText;
+
+        static void Main(string[] args)
+        {
+            cY = 1353000L; // Set the Y coord for the first pic 
+            Count = 0;
+
+            headerText = "Test";
+            contentText = "Some content.";
+
+
+            string sourceFile = @"D:\Projects\word-doc\PresentationProject\Presentation1.pptx";
+            string destFile = @"D:\Projects\word-doc\PresentationProject\Presentation1-used.pptx";
+            System.IO.File.Copy(sourceFile, destFile, true);
+
+
+            GetAllTextInSlide(destFile, 0);
+
+            string anotherPic = @"D:\Projects\word-doc\image.jpg";
+            InsertAPicture(destFile, 0, anotherPic);
+
+            string anotherPic2 = @"D:\Projects\word-doc\image-old.jpg";
+            InsertAPicture(destFile, 0, anotherPic2);
+
+            string anotherPic3 = @"D:\Projects\word-doc\image2.jpg";
+            InsertAPicture(destFile, 0, anotherPic3);
+
+        }
+
+
+        public static void GetAllTextInSlide(string presentationFile, int slideIndex)
+        {
+            using (PresentationDocument presentationDocument = PresentationDocument.Open(presentationFile, true))
+            {
+                GetAllTextInSlide(presentationDocument, slideIndex);
+            }
+        }
+
+        public static void GetAllTextInSlide(PresentationDocument presentationDocument, int slideIndex)
+        {
+            PresentationPart presentationPart = presentationDocument.PresentationPart;
+
+            Presentation presentation = presentationPart.Presentation;
+
+            if (presentation.SlideIdList != null)
+            {
+                DocumentFormat.OpenXml.OpenXmlElementList slideIds = presentation.SlideIdList.ChildElements;
+
+                if (slideIndex < slideIds.Count)
+                {
+                    string slidePartRelationshipId = (slideIds[slideIndex] as SlideId).RelationshipId;
+                    SlidePart slidePart = (SlidePart)presentationPart.GetPartById(slidePartRelationshipId);
+
+                    GetAllTextInSlide(slidePart, headerText, contentText);
+                }
+
+            }
+
+            // return null;
+
+        }
+
+        public static void GetAllTextInSlide(SlidePart slidePart, string header, string content)
+        {
+
+            if (slidePart.Slide != null)
+            {
+                /*Header*/
+                DocumentFormat.OpenXml.Drawing.Paragraph paragraph = slidePart.Slide.Descendants<DocumentFormat.OpenXml.Drawing.Paragraph>().First();
+
+                DocumentFormat.OpenXml.Drawing.Text text = paragraph.Descendants<DocumentFormat.OpenXml.Drawing.Text>().First();
+                text.Text = header;
+
+
+                DocumentFormat.OpenXml.Drawing.TableCell cell = slidePart.Slide.Descendants<DocumentFormat.OpenXml.Drawing.TableCell>().First();
+
+                /*Content*/
+                DocumentFormat.OpenXml.Drawing.Text t = cell.Descendants<DocumentFormat.OpenXml.Drawing.Text>().First();
+                t.Text = content;
+            }
+        }
+
+        /*Image*/
+        public static void InsertAPicture(string document, int slideIndex, string fileName)
+        {
+            using (PresentationDocument presentationDocument = PresentationDocument.Open(document, true))
+            {
+                PresentationPart presentationPart = presentationDocument.PresentationPart;
+
+                Presentation presentation = presentationPart.Presentation;
+
+                if (presentation.SlideIdList != null)
+                {
+                    DocumentFormat.OpenXml.OpenXmlElementList slideIds = presentation.SlideIdList.ChildElements;
+
+                    if (slideIndex < slideIds.Count)
+                    {
+                        string slidePartRelationshipId = (slideIds[slideIndex] as SlideId).RelationshipId;                    
+
+                        SlidePart slidePart = (SlidePart)presentationPart.GetPartById(slidePartRelationshipId);
+
+                        Slide slide = slidePart.Slide;
+
+                        InsertImageInLastSlide(slidePart, slide, fileName);
+                        
+                    }
+
+                }
+            }
+        }
+
+
+
+        public static void InsertImageInLastSlide(SlidePart slidePart, Slide slide, string imagePath)
+        {
+            // Creates a Picture instance and adds its children. 
+            P.Picture picture = new P.Picture();
+            string embedId = string.Empty;
+            embedId = "rId" + (slide.Elements<P.Picture>().Count() + 915 + Count).ToString();
+            P.NonVisualPictureProperties nonVisualPictureProperties = new P.NonVisualPictureProperties(
+                new P.NonVisualDrawingProperties() { Id = (UInt32Value)4U, Name = "Picture 5" },
+                new P.NonVisualPictureDrawingProperties(new A.PictureLocks() { NoChangeAspect = true }),
+                new ApplicationNonVisualDrawingProperties());
+
+            P.BlipFill blipFill = new P.BlipFill();
+            Blip blip = new Blip() { Embed = embedId };
+
+            // Creates a BlipExtensionList instance and adds its children 
+            BlipExtensionList blipExtensionList = new BlipExtensionList();
+            BlipExtension blipExtension = new BlipExtension() { Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}" };
+
+            UseLocalDpi useLocalDpi = new UseLocalDpi() { Val = false };
+            useLocalDpi.AddNamespaceDeclaration("a14",
+                "http://schemas.microsoft.com/office/drawing/2010/main");
+
+            blipExtension.Append(useLocalDpi);
+            blipExtensionList.Append(blipExtension);
+            blip.Append(blipExtensionList);
+
+            Stretch stretch = new Stretch();
+            FillRectangle fillRectangle = new FillRectangle();
+            stretch.Append(fillRectangle);
+
+            blipFill.Append(blip);
+            blipFill.Append(stretch);
+
+            // Creates a ShapeProperties instance and adds its children. 
+            P.ShapeProperties shapeProperties = new P.ShapeProperties();
+
+            A.Transform2D transform2D = new A.Transform2D();
+            A.Offset offset = new A.Offset() { X = 9144000L, Y = cY };
+
+            A.Extents extents = new A.Extents() { Cx = 2057400L, Cy = 1257300L };
+
+            transform2D.Append(offset);
+            transform2D.Append(extents);
+
+            A.PresetGeometry presetGeometry = new A.PresetGeometry() { Preset = A.ShapeTypeValues.Rectangle };
+            A.AdjustValueList adjustValueList = new A.AdjustValueList();
+
+            presetGeometry.Append(adjustValueList);
+
+            shapeProperties.Append(transform2D);
+            shapeProperties.Append(presetGeometry);
+
+            picture.Append(nonVisualPictureProperties);
+            picture.Append(blipFill);
+            picture.Append(shapeProperties);
+
+            slide.CommonSlideData.ShapeTree.AppendChild(picture);
+
+            ImagePart imagePart = slidePart.AddImagePart(ImagePartType.Jpeg, embedId);
+            using (FileStream stream = new FileStream(imagePath, FileMode.Open))
+            {
+                imagePart.FeedData(stream);
+            }
+
+            cY += 1571625;
+            Count++;
+        }
+    }
+}
---
+++ myprojects/msWord/WordProject/WordProject/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.IO.Packaging;
+using System.IO;
+using A = DocumentFormat.OpenXml.Drawing;
+using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
+using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
+
+
+
+namespace WordProject
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string header = "HeaderTest";
+            
+            string content = "Some content.";
+
+            string sourceFile = @"D:\Projects\word-doc\Doc3.docx";
+            string destFile = @"D:\Projects\word-doc\Doc-used.docx";
+            System.IO.File.Copy(sourceFile, destFile, true);
+
+            ChangeHeader(destFile, header);
+
+            WriteContent(destFile, content);
+
+            string fileName = @"D:\Projects\word-doc\image-old.jpg";
+            InsertAPicture(destFile, fileName);
+
+            string anotherPic = @"D:\Projects\word-doc\image.jpg";
+            InsertAPicture(destFile, anotherPic);
+
+            string fileName2 = @"D:\Projects\word-doc\image-old2.jpg";
+            InsertAPicture(destFile, fileName2);
+            
+         }
+
+        /*Image*/
+        public static void InsertAPicture(string document, string fileName)
+        {
+            using (WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(document, true))
+            {
+                MainDocumentPart mainPart = wordprocessingDocument.MainDocumentPart;
+
+                ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+
+                using (FileStream stream = new FileStream(fileName, FileMode.Open))
+                {
+                    imagePart.FeedData(stream);
+                }
+
+                AddImageToBody(wordprocessingDocument, mainPart.GetIdOfPart(imagePart));
+            }
+        }
+
+        private static void AddImageToBody(WordprocessingDocument wordDoc, string relationshipId)
+        {
+            var element =
+        new Drawing(
+            new DW.Inline(
+                new DW.Extent() { Cx = 990000L, Cy = 792000L },
+                new DW.EffectExtent()
+                {
+                    LeftEdge = 0L,
+                    TopEdge = 0L,
+                    RightEdge = 0L,
+                    BottomEdge = 0L
+                },
+                new DW.DocProperties()
+                {
+                    Id = (UInt32Value)1U,
+                    Name = "Picture 1"
+                },
+                new DW.NonVisualGraphicFrameDrawingProperties(
+                    new A.GraphicFrameLocks() { NoChangeAspect = true }),
+                new A.Graphic(
+                    new A.GraphicData(
+                        new PIC.Picture(
+                            new PIC.NonVisualPictureProperties(
+                                new PIC.NonVisualDrawingProperties()
+                                   {
+                                       Id = (UInt32Value)0U,
+                                       Name = "New Bitmap Image.jpg"
+                                   },
+                                new PIC.NonVisualPictureDrawingProperties()),
+                            new PIC.BlipFill(
+                                new A.Blip(
+                                    new A.BlipExtensionList(
+                                        new A.BlipExtension()
+                                           {
+                                               Uri =
+                                                 "{28A0092B-C50C-407E-A947-70E740481C1C}"
+                                           })
+                                )
+                                {
+                                    Embed = relationshipId,
+                                    CompressionState =
+                                    A.BlipCompressionValues.Print
+                                },
+                                new A.Stretch(
+                                    new A.FillRectangle())),
+                            new PIC.ShapeProperties(
+                                new A.Transform2D(
+                                    new A.Offset() { X = 0L, Y = 0L },
+                                    new A.Extents() { Cx = 990000L, Cy = 792000L }),
+                                new A.PresetGeometry(
+                                    new A.AdjustValueList()
+                                ) { Preset = A.ShapeTypeValues.Rectangle }))
+                    ) { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
+            )
+            {
+                DistanceFromTop = (UInt32Value)0U,
+                DistanceFromBottom = (UInt32Value)0U,
+                DistanceFromLeft = (UInt32Value)0U,
+                DistanceFromRight = (UInt32Value)0U,
+                EditId = "50D07946"
+            });
+
+            //Insert to the 2nd column
+            Table table = wordDoc.MainDocumentPart.Document.Body.Elements<Table>().First();
+            TableRow row = table.Elements<TableRow>().ElementAt(0);
+            TableCell cell = row.Elements<TableCell>().ElementAt(1);
+
+            Paragraph p = cell.AppendChild(new Paragraph());
+            Run r = p.AppendChild(new Run());
+            r.AppendChild(element);
+
+            //Add an extra line break
+            cell.AppendChild(new Paragraph());
+        }
+
+        public static void WriteContent(string filepath, string txt)
+        {
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(filepath, true))
+            {
+                Table table = doc.MainDocumentPart.Document.Body.Elements<Table>().First();
+                TableRow row = table.Elements<TableRow>().First();
+                TableCell cell = row.Elements<TableCell>().First();
+                SdtBlock block = cell.Elements<SdtBlock>().First();
+                SdtContentBlock container = block.Elements<SdtContentBlock>().First();
+                Paragraph p = container.Elements<Paragraph>().First();
+                Run r = p.Elements<Run>().First();
+                Text t = r.Elements<Text>().First();
+                t.Text = txt;
+            }
+        }
+
+        public static void ChangeHeader(string filepath, string txt)
+        {
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(filepath, true))
+            {
+                Paragraph p = doc.MainDocumentPart.Document.Body.Elements<Paragraph>().First();
+                Run r = p.Elements<Run>().First();
+                Text t = r.Elements<Text>().First();
+                t.Text = txt;
+            }
+        }
+
+
+    }
+}
